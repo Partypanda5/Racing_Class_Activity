61ee4c7083db900505aafeb46549ed6cba696186
Andrea Hayes <scoondi@hotmail.com>
2023-09-13 21:14:09 +0200

    Update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,46 @@ using UnityEngine.InputSystem;
 [RequireComponent (typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Character Controller")]
     private CharacterController controller;
-   [SerializeField] private float playerSpeed = 2.0f;
+
+    [Header("Character Settings")]
+    [Space(10)]
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float gravityValue = -9.81f;
+
+    [Header("Bullet Settings")]
+    [Space(10)]
+    [SerializeField] private float bulletSpeed = 5;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firePoint;
+
+    [Header("Vector 3s")]
+    private Vector3 playerVelocity;
     private Vector2 _playerInput;
+
+    [Header("Bools")]
+    private bool groundedPlayer;
+    private bool jump;
+    private bool shoot;
+
 
     public void playerMove(InputAction.CallbackContext context)
     {
         
         _playerInput = context.ReadValue<Vector2>();
+    }
+
+    public void playerJump(InputAction.CallbackContext context) 
+    {
+        jump = context.action.triggered;
+        Debug.Log(jump);
+    }
+
+    public void playerShoot(InputAction.CallbackContext context) 
+    {
+        shoot = context.action.triggered;
     }
 
     private void Start()
@@ -21,8 +53,31 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
         Vector3 move = new Vector3(_playerInput.y * -1, 0, _playerInput.x);
         controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (jump && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        if (shoot) 
+        {
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody Bulletrb = bullet.GetComponent<Rigidbody>();
+            Bulletrb.AddForce(firePoint.forward * bulletSpeed, ForceMode.Impulse);
+
+        }
     }
 
 
--- Assets/Scripts/SpawnPlayers.cs
+++ Assets/Scripts/SpawnPlayers.cs
@@ -1,24 +1,29 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class SpawnPlayers : MonoBehaviour
 {
-    public GameObject playerOnePrefab;
-    public GameObject playerTwoPrefab;
-
-    public void SpawnPlayerOne() 
-    {
-       PlayerInput.Instantiate(prefab: playerOnePrefab, playerIndex: 0, controlScheme: "Player01", pairWithDevice: Keyboard.current, splitScreenIndex: 0);
-    }
-
-    public void SpawnPlayerTwo() 
-    {
-        PlayerInput.Instantiate(prefab: playerTwoPrefab, playerIndex: 1, controlScheme: "Player02", pairWithDevice: Keyboard.current, splitScreenIndex: 1);
-    }
+    int index;
+    public List<GameObject> players = new List<GameObject>();
+    PlayerInputManager MultiplayerManager;
 
     private void Start()
     {
-        SpawnPlayerOne();
-        SpawnPlayerTwo();
+        MultiplayerManager = GetComponent<PlayerInputManager>();
+        index = 0;
+        MultiplayerManager.playerPrefab = players[index];
+
+        //PlayerInput.Instantiate(prefab: playerOnePrefab, playerIndex: 0, controlScheme: "Controller", pairWithDevice: Gamepad.current, splitScreenIndex: 0);
+        //PlayerInput.Instantiate(prefab: playerTwoPrefab, playerIndex: 1, controlScheme: "Player01", pairWithDevice: Keyboard.current, splitScreenIndex: 1);
+    }
+
+    public void spawnNextPlayer(PlayerInput input) 
+    {
+        index++;
+        if (index >= players.Count) 
+        { 
+            index = 0; 
+        }
     }
 }
